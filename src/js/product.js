f57f95898b84f892de2ac5b86d28140964cde91f
gshaymond <gshaymond@byui.edu>
2025-01-21 14:16:58 -0700

    Co-authored-by: Mitchell Brandt Bower <mbower00@users.noreply.github.com> Co-authored-by: naa-bersa <naa-bersa@users.noreply.github.com>

--- src/js/product.js
+++ src/js/product.js
@@ -1,16 +1,16 @@
-import { setLocalStorage } from "./utils.mjs";
-import { findProductById } from "./productData.mjs";
+import { getParam} from "./utils.mjs";
+import { productDetails } from "./productDetails.mjs";
 
-function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
-}
+const productId = getParam("product");
+productDetails(productId, ".product-detail");
+
 // add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+// async function addToCartHandler(e) {
+//   const product = await findProductById(e.target.dataset.id);
+//   addProductToCart(product);
+// }
 
 // add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+// document
+//   .getElementById("addToCart")
+//   .addEventListener("click", addToCartHandler);
---
+++ src/js/productDetails.mjs
@@ -0,0 +1,13 @@
+let productData
+
+export function productDetails(productId, selector) {
+    
+}
+
+function addProductToCart(product) {
+  setLocalStorage("so-cart", product);
+}
+
+function productDetailsTemplate(product) {
+
+}
