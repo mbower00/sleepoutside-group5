ffc58b163879322bcd59e8417885dc312d3977dd
gshaymond <gshaymond@byui.edu>
2025-01-30 13:52:21 -0700

    dinamic details

--- src/js/product.js
+++ src/js/product.js
@@ -1,16 +1,5 @@
 import { getParam} from "./utils.mjs";
-import { productDetails } from "./productDetails.mjs";
+import productDetails from "./productDetails.mjs";
 
 const productId = getParam("product");
 productDetails(productId, ".product-detail");
-
-// add to cart button event handler
-// async function addToCartHandler(e) {
-//   const product = await findProductById(e.target.dataset.id);
-//   addProductToCart(product);
-// }
-
-// add listener to Add to Cart button
-// document
-//   .getElementById("addToCart")
-//   .addEventListener("click", addToCartHandler);
--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -1,13 +1,48 @@
-let productData
+import {findProductById} from  "./productData.mjs";
 
-export function productDetails(productId, selector) {
-    
+export default async function productDetails(productId, selector) {
+  // use findProductById to get the details for the current product. findProductById will return a promise! use await or .then() to process it
+  let productData = await findProductById(productId);
+  // once we have the product details we can render out the HTML
+  document.querySelector(".product-detail").innerHTML = productDetailsTemplate(productData)
+  // add a listener to Add to Cart button
+  document
+    .getElementById("addToCart")
+    .addEventListener("click", addToCartHandler);
+}
+
+// add to cart button event handler
+async function addToCartHandler(e) {
+  const product = await findProductById(e.target.dataset.id);
+  addProductToCart(product);
 }
 
 function addProductToCart(product) {
   setLocalStorage("so-cart", product);
+  
 }
 
 function productDetailsTemplate(product) {
+  console.log(product)
+  return `
+    <h3>${product.Brand.Name}</h3>
 
+    <h2 class="divider">${product.NameWithoutBrand}</h2>
+
+    <img
+      class="divider"
+      src="${product.Image}"
+      alt="${product.NameWithoutBrand}"
+    />
+
+    <p class="product-card__price">${product.FinalPrice}</p>
+
+    <p class="product__color">${product.Colors[0].ColorName}</p>
+
+    <p class="product__description">${product.DescriptionHtmlSimple}</p>
+
+    <div class="product-detail__add">
+      <button id="addToCart" data-id="${product.Id}">Add to Cart</button>
+    </div>
+  `;
 }
