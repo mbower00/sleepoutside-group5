24750230fa4e3543c23867c0703774ef0e67614b
mbower00 <mitchbbower@gmail.com>
2025-02-20 11:55:05 -0700

    current team progress

---
+++ src/js/product-list.js
@@ -0,0 +1,11 @@
+import ProductList from "./components/ProductList.svelte";
+import { mount } from "svelte";
+import { renderHeaderFooter } from "./utils.mjs";
+
+renderHeaderFooter()
+const productList = mount(ProductList, {
+  target: document.querySelector(".products"),
+  props: {
+    category: "tents",
+  },
+});
