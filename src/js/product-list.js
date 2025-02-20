7b3172a4d68e134ee44f951cade7ea3c577e97e7
mbower00 <mitchbbower@gmail.com>
2025-02-20 13:03:43 -0700

    format

--- src/js/product-list.js
+++ src/js/product-list.js
@@ -2,8 +2,8 @@ import ProductList from "./components/ProductList.svelte";
 import { mount } from "svelte";
 import { renderHeaderFooter, getParam } from "./utils.mjs";
 
-renderHeaderFooter()
-const category = getParam("category")
+renderHeaderFooter();
+const category = getParam("category");
 const productList = mount(ProductList, {
   target: document.querySelector(".products"),
   props: {
