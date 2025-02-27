94fc7a4949397395696e0f11ded8163b55948dd5
gshaymond <gshaymond@byui.edu>
2025-02-27 14:20:05 -0700

    Form calculations

--- src/js/components/CheckoutForm.svelte
+++ src/js/components/CheckoutForm.svelte
@@ -25,18 +25,30 @@
     event.target.reset();
   }
 
+  function zipCodeInputHandler(e) {
+    if (`${zip}`.length === 5) {
+      calculateShippingTaxTotal()
+    }
+  }
+
   function calculateItemSummary() {
     return cart
       .reduce((sum, item) => sum + item.FinalPrice * (item.Qty || 1), 0)
       .toFixed(2);
   }
+
   function calculateShippingTaxTotal() {
-    // TODO: write this function and call it when zip is added
-    return;
+    // calculate shipping
+    estimate = 10 + (2 * (count - 1))
+    // calculate tax
+    tax = (subtotal * 0.06).toFixed(2);
+    // calculate total
+    total = Number(subtotal) + Number(estimate) + Number(tax)
   }
+
+
   function init() {
-    console.log(cartKey);
-    let cart = getLocalStorage(cartKey);
+    cart = getLocalStorage(cartKey);
     count = cart.reduce((sum, item) => sum + 1 * (item.Qty || 1), 0);
     subtotal = calculateItemSummary();
   }
@@ -57,19 +69,19 @@
     />
 
     <label for="last-name"> Last Name </label>
-    <input type="text" id="last-name" name="last-name" bind:value={lastName} />
+    <input type="text" required id="last-name" name="last-name" bind:value={lastName} />
 
     <label for="street"> Street </label>
-    <input type="text" id="street" name="street" bind:value={street} />
+    <input type="text" required id="street" name="street" bind:value={street} />
 
     <label for="city"> City </label>
-    <input type="text" id="city" name="city" bind:value={city} />
+    <input type="text" required id="city" name="city" bind:value={city} />
 
     <label for="state"> State </label>
-    <input type="text" id="state" name="state" bind:value={state} />
+    <input type="text" required id="state" name="state" bind:value={state} />
 
     <label for="zip"> Zip </label>
-    <input type="text" id="zip" name="zip" bind:value={zip} />
+    <input type="text" required maxlength="5" id="zip" name="zip" bind:value={zip} onchange={zipCodeInputHandler} />
   </fieldset>
 
   <!-- card info -->
@@ -112,6 +124,7 @@
       <p>Order Total <span class="summary-number">${total}</span></p>
     </strong>
   </fieldset>
+  <button type="submit">Submit</button>
 </form>
 
 <style>
